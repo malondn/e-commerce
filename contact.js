1202dae2e2e71209f2d67e5dd7eb14fe976ce9e7
malondn <malondnainggolan@yahoo.com>
2025-05-04 09:39:05 +0000

    modified: contact.html modified: contact.js

--- contact.js
+++ contact.js
@@ -1,3 +1,11 @@
 document.querySelector("form").addEventListener("submit", function(event) {
     alert("Thank you! We’ll get back to you shortly.");
   });
+
+document.getElementById("home-link").addEventListener("click", function() {
+  window.location.href = "index.html";
+})
+
+document.getElementById("shop-link").addEventListener("click", function() {
+  window.location.href = "shop.html";
+})
