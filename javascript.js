8517880549e4989d7d5360ac5600b4fc55af3c2e
malondn <malondnainggolan@yahoo.com>
2025-05-04 08:51:34 +0000

    modified: javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
 document.getElementById("shopButton").addEventListener("click", function() {
     window.location.href = "shop.html"; // Redirect to the shop page
   });
+
+  document.getElementById("shop-link").addEventListener("click", function() {
+    window.location.href = "shop.html";
+  });
