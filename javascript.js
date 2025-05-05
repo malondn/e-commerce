ce8c2ff5e7572b74b1d258cc5d09e436876439e6
malondn <malondnainggolan@yahoo.com>
2025-05-05 04:35:00 +0000

    new file: casioGshockBlack modified: javascript.js modified: styles.css

--- javascript.js
+++ javascript.js
@@ -13,3 +13,23 @@ document.getElementById("shopButton").addEventListener("click", function() {
   document.querySelector("form").addEventListener("submit", function(event) {
     alert("Thank you! We’ll get back to you shortly.");
   });
+
+  /* casioGshockblack image slideshow */
+  const images = [
+    "https://images.pexels.com/photos/280250/pexels-photo-280250.jpeg?cs=srgb&dl=pexels-pixabay-280250.jpg&fm=jpg",
+    "https://media.istockphoto.com/id/650233226/photo/military-style-watch.jpg?s=612x612&w=0&k=20&c=PknuHX2Dh0SN2JTKF60Ftxowdiwoudc_zTu9Vd2UD-w=",
+    "https://cdn.pixabay.com/photo/2022/08/29/15/11/clock-7419071_640.jpg"
+  ];
+  
+  let currentIndex = 0;
+  const slideImg = document.getElementById("slide-img");
+  
+  document.getElementById("prev-btn").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    slideImg.src = images[currentIndex];
+  });
+  
+  document.getElementById("next-btn").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    slideImg.src = images[currentIndex];
+  });
